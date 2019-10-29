2f7828af698f0a966a01ce26ed5392b18687077b
Artem <>
2019-10-28 22:10:36 -0400

    Ex6: Added bee.move()

--- exercises/broken-game-oop-predator-prey/js/Prey.js
+++ exercises/broken-game-oop-predator-prey/js/Prey.js
@@ -61,7 +61,7 @@ class Prey {
     // wraps it to the other side if so
     handleWrapping() {
       // Off the left or right
-      if (this.x < 0) {
+      if (this.x < 0) { //FIXED > to <, caused prey not to move
         this.x += width;
       }
       else if (this.x > width) {
